5bfa909631eb4e2ecf707a40128c0b974f988e86
Frédéric Bérubé <fredob@hotmail.ca>
2017-02-02 08:38:09 -0500

    Adding Subpos (maybe temp placement)

--- app/src/main/java/com/example/julien/geoapp/MainActivity.java
+++ app/src/main/java/com/example/julien/geoapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.julien.geoapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.example.julien.geoapp.SubPos.SubPos;
 import com.mapbox.mapboxsdk.MapboxAccountManager;
 import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -13,19 +15,44 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private static final String TAG = "DrawGeojsonLineActivity";
 
+    private static final String LAT_TAG = "POSITION'S LATITUDE";
+    private static final String LONG_TAG = "POSITION'S LONGITUDE";
+    private static final String SUBPOS_TAG = "POSITION'S CREATED";
+
     private MapView mapView;
     private MapboxMap mapboxMap;
+
+    //region SubPos Variables
+    private SubPos subpos;
+    private double posLongitude;
+    private double posLatitude;
+    //endregion
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MapboxAccountManager.start(this, getString(R.string.access_token));
-
+         MapboxAccountManager.start(this, getString(R.string.access_token));
         setContentView(R.layout.activity_main);
+
+        this.subpos = new SubPos(this);
+
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+
+        this.getMyPosition();
     }
+
+    //region SUBPOS Functions
+    public void getMyPosition(){
+        Log.d(SUBPOS_TAG, String.valueOf(this.subpos.getNodes()));
+        this.posLatitude = this.subpos.getPosition().getLat();
+        this.posLongitude = this.subpos.getPosition().getLong();
+
+        Log.d(LAT_TAG, String.valueOf(this.posLatitude));
+        Log.d(LONG_TAG, String.valueOf(this.posLongitude));
+    }
+    //endregion
 
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
