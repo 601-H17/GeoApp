54464af94d8976a622d9d3b5b0feb7afcf76547d
laitauchocolat <turcotte.julien15@gmail.com>
2017-02-02 11:04:37 -0500

    sick dank

--- app/src/main/java/com/example/julien/geoapp/MainActivity.java
+++ app/src/main/java/com/example/julien/geoapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.julien.geoapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.arlib.floatingsearchview.FloatingSearchView;
 import com.mapbox.mapboxsdk.MapboxAccountManager;
 import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private MapView mapView;
     private MapboxMap mapboxMap;
+    private FloatingSearchView mSearchView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,15 +25,35 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         MapboxAccountManager.start(this, getString(R.string.access_token));
 
         setContentView(R.layout.activity_main);
+        mSearchView = (FloatingSearchView)findViewById(R.id.mSearchView);
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
     }
 
+//    @Overridel
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView = (SearchView) menu.findItem(R.id.search_bar).getActionView();
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+//        searchView.setIconifiedByDefault(false);
+//        return true;
+//    }
+//
+//    mSearchView.setOnMenuItemClickListener(new FloatingSearchView.OnMenuItemClickListener() {
+//        @Override
+//        public void onMenuItemSelected(MenuItem item) {
+//
+//        }
+//    });
+
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
         new DrawGeoJson(mapboxMap,this).execute();
+
     }
 
     @Override
