d1005c111177a77755d29833431b8c3fdb3bb706
Frédéric Bérubé <fredob@hotmail.ca>
2017-02-02 12:30:10 -0500

    Ajout des coordonnées centrales, et point sur la carte

--- app/src/main/java/com/example/julien/geoapp/MainActivity.java
+++ app/src/main/java/com/example/julien/geoapp/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.julien.geoapp;
+import android.animation.ObjectAnimator;
+import android.animation.TypeEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
 import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Projection;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -18,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private MapView mapView;
     private MapboxMap mapboxMap;
     private FloatingSearchView mSearchView;
+
+    private LatLng centerCoordinates;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.getMapAsync(this);
     }
 
-//    @Overridel
+//    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        // Inflate the menu; this adds items to the action bar if it is present.
 //        getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -50,10 +61,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 //    });
 
     @Override
-    public void onMapReady(MapboxMap mapboxMap) {
+    public void onMapReady(final MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
         new DrawGeoJson(mapboxMap,this).execute();
 
+        final Projection projection = mapboxMap.getProjection();
+        final int width = mapView.getMeasuredWidth();
+        final int height = mapView.getMeasuredHeight();
+
+        this.setCenterCoordinates(width, height, projection);
+        this.mapboxMap.setOnCameraChangeListener(new MapboxMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition position) {
+                setCenterCoordinates(width, height, projection);
+            }
+        });
+
+    }
+
+    private void setCenterCoordinates(int width, int height, Projection projection){
+        PointF centerPoint = new PointF(width / 2, height / 2);
+        this.centerCoordinates = new LatLng(projection.fromScreenLocation(centerPoint));
     }
 
     @Override
