73f150554f5eab3eea1cc0f23265096a3f0ceece
Frédéric Bérubé <fredob@hotmail.ca>
2017-02-08 10:28:42 -0500

    Refactoring .yml

--- set_tags.sh
+++ set_tags.sh
@@ -12,7 +12,6 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
 
       git config --global user.email "irakotoseheno@gmail.com"
 	    git config --global user.name "eric3475"
-      git remote add origin https://eric3475:${GH_TOKEN}@github.com/601-H17/GeoApp.git
 
       # Add tag and push to master.
       git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
