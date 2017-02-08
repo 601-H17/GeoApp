3342b4c7f1ce4d168615b7974934b334293355d5
Frédéric Bérubé <fredob@hotmail.ca>
2017-02-08 12:47:35 -0500

    Creating first UI Test

--- app/src/androidTest/java/com/example/julien/geoapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/julien/geoapp/ExampleInstrumentedTest.java
@@ -2,11 +2,18 @@ package com.example.julien.geoapp;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +29,16 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.julien.geoapp", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    //UI TEST EXAMPLE
+    @Test
+    public void mapView_IsDisplayed(){
+        ViewInteraction mapView = onView(withId(R.id.mapview));
+        mapView.check(matches(isDisplayed()));
+
     }
 }
