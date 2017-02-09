565f534d697ed52471cab935fdf1607242e14069
eric3475 <irakotoseheno@gmail.com>
2017-02-09 08:51:30 -0500

    without ui tests

--- app/src/androidTest/java/com/example/julien/geoapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/julien/geoapp/ExampleInstrumentedTest.java
@@ -1,26 +1,33 @@
-package com.example.julien.geoapp;
-
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
+
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.julien.geoapp", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    //UI TEST EXAMPLE
+    @Test
+    public void mapView_IsDisplayed(){
+        ViewInteraction mapView = onView(withId(R.id.mapview));
+        mapView.check(matches(isDisplayed()));
+
     }
 }
