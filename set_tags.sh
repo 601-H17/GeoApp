eba71702e694fba5f050990e7739ee42bb721269
Frédéric Bérubé <fredob@hotmail.ca>
2017-02-15 09:15:30 -0500

    Testing release

--- set_tags.sh
+++ set_tags.sh
@@ -13,6 +13,7 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
       git config --global user.email "irakotoseheno@gmail.com"
 	    git config --global user.name "eric3475"
 
+#Test
       # Add tag and push to master.
       git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
       git push origin --tags
