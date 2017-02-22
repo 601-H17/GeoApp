9446e95b8955b0ae4fec2bd87e251952a132b036
eric3475 <irakotoseheno@gmail.com>
2017-02-22 09:50:55 -0500

    ui tests should works

--- app/src/androidTest/java/com/example/julien/geoapp/activity/activityTest/MainActivityTest/MainActivityUiTest.java
+++ app/src/androidTest/java/com/example/julien/geoapp/activity/activityTest/MainActivityTest/MainActivityUiTest.java
@@ -1,36 +1,20 @@
-package com.example.julien.geoapp.activity;
+package com.example.julien.geoapp.activity.activityTest.MainActivityTest;
 
 
-import android.support.test.espresso.ViewAssertion;
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.assertion.ViewAssertions.*;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
 
 import com.example.julien.geoapp.R;
+import com.example.julien.geoapp.activity.MainActivity;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 
 /**
@@ -43,6 +27,8 @@ public class MainActivityUiTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+
 
     @Test
     public void seeAllFloorButtons1() {
@@ -61,4 +47,13 @@ public class MainActivityUiTest {
         onView(withId(R.id.button3))
                 .check(ViewAssertions.matches(isDisplayed()));
     }
+
+    @Test
+    public void seeSearchMenu() {
+        onView(withId(R.id.searchMenu))
+                .check(ViewAssertions.matches(isDisplayed()));
+    }
+
+
+
 }
