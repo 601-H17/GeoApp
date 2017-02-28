4b5c3ab453d03550265fb7ee99f363559c713022
laitauchocolat <turcotte.julien15@gmail.com>
2017-02-28 10:36:54 -0500

    delted test

---
+++ app/src/main/java/com/example/julien/geoapp/Externalization/Message.java
@@ -0,0 +1,21 @@
+package com.example.julien.geoapp.Externalization;
+
+/**
+ * Created by Julien on 2017-02-28.
+ */
+
+public class Message {
+
+    public static String[] FEATURES_JSON = {"features", "geometry", "type", "Point", "coordinates", "properties", "ref", "entrance", "doors: ", "path"};
+    public static String REQUEST_NULL = "";
+    public static String ENTER = "\n";
+    public static String[] FEATURES_JSON_PATH = {"name", "description", "floor", "point", "lat", "lng"};
+    public static String[] ERROR = {"TAG", "Exception Loading GeoJSON "};
+    public static String[] ENTITY_TYPE = {"doors", "stairs", "image"};
+    public static String PATH_FILE = "data/data/com.example.julien.geoapp/files/";
+    public static String EXETENSIOM = ".png";
+    public static String TO_MARKER = ".png";
+    public static String COLOR_WALL = "#ffffff";
+    public static String COLOR_PPATH = "#cb2c39";
+
+}
