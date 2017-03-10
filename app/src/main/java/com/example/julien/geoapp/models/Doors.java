decc71e3f286afbfaf0253ce460ae669fc4d2e4f
eric3475 <irakotoseheno@gmail.com>
2017-03-09 19:20:11 -0500

    tags all done

--- app/src/main/java/com/example/julien/geoapp/models/Doors.java
+++ app/src/main/java/com/example/julien/geoapp/models/Doors.java
@@ -1,5 +1,7 @@
 package com.example.julien.geoapp.models;
 
+import java.util.List;
+
 /**
  * Created by Julien on 2017-02-10.
  */
@@ -7,16 +9,16 @@ package com.example.julien.geoapp.models;
 public class Doors extends Point {
     private int etage;
     private String teacher;
-    private String tag;
+    private List<String> tag;
 
-    public Doors(String Title, String Description, String teacher, int etage, double lati, double longi, String tagForDoor) {
+    public Doors(String Title, String Description, String teacher, int etage, double lati, double longi, List<String> tagForDoor) {
         super(Title, Description, lati, longi);
         this.etage = etage;
         this.teacher = teacher;
         this.tag = tagForDoor;
     }
 
-    public String getTag() {
+    public List<String> getTags() {
         return tag;
     }
     public int getEtage() {
